bf94d955e718c214ed3ef1a3bad3b5ccb6e41d13
barisdemirkap <barisdemirkap@gmail.com>
2022-01-01 09:07:52 +0300

    for loops

--- Program.cs
+++ Program.cs
@@ -4,12 +4,54 @@ namespace donguler_for_loop
     class Program
     {
         static void Main(string[] args)
-        {
-            for (int i=0 ; i < length; i++)
+        {   
+            //Ekrana girilen sayiya kadar olan tek sayilari ekrana yazdir.
+            Console.WriteLine("Lutfen bir sayi giriniz") ;
+            int _counter = int.Parse(Console.ReadLine()) ;
+            for (int i=0 ; i <= _counter; i++)
             {
-                //komutlar
+                if(i%2==1)
+                    Console.WriteLine(i) ;
             }
+
+
+            //1 ile 1000 arasindaki tek ve cift sayilarin kendiclerinde toplamlarinin ekrana yazdir.
+
+            int oddsum = 0;
+            int evensum= 0 ;
+            for(int i=1 ; i<=1000 ; i++)
+            {
+                if(i%2==1)
+                    oddsum+= i ; //tekToplam = tektoplam + i 
+
+                else
+                    evensum+= i ; // cifttoplam= cift toplam + i
+            }
+
+            Console.WriteLine("OddSum:"+ oddsum) ;
+            Console.WriteLine("EvenSum:"+ evensum) ;
+
+            //break, continue 
+            for (int i = 1 ; i<10 ; i++)
+                {
+                    if(i==4)
+                        break;
+                    Console.WriteLine(i ) ;
+                }
+            for(int i = 1 ; i <10 ; i++)
+                {
+                    if (i==4)    
+                        continue ;
+                    Console.WriteLine(i ) ;
+
+                }
+
+
+
+
+        }   
+
+
         }
     }
 
-}
