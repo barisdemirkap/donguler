2058f953864b213cc9e9ba024d16efb9a8cdead8
barisdemirkap <barisdemirkap@gmail.com>
2022-01-01 08:46:04 +0300

    initial commit of for loop

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace donguler_for_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i=0 ; i < length; i++)
+            {
+                //komutlar
+            }
+        }
+    }
+
+}
